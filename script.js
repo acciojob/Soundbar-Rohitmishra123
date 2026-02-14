27044cca3a6d0c93631348f50b059724fe59aa53
Rohit Kumar Mishra <105963356+Rohitmishra123@users.noreply.github.com>
2026-02-14 18:25:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+const sounds = ["clap", "kick", "hihat", "boom"];
+
+const buttonsContainer = document.getElementById("buttons");
+
+let currentAudio = null;
+
+// Create buttons dynamically
+sounds.forEach(sound => {
+    const button = document.createElement("button");
+    button.innerText = sound.toUpperCase();
+    button.classList.add("btn");
+
+    button.addEventListener("click", () => {
+
+        if (currentAudio) {
+            currentAudio.pause();
+            currentAudio.currentTime = 0;
+        }
+
+        const audio = new Audio(`sounds/${sound}.mp3`);
+        currentAudio = audio;
+        audio.play();
+    });
+
+    buttonsContainer.appendChild(button);
+});
+
+// Stop Button
+const stopButton = document.createElement("button");
+stopButton.innerText = "STOP";
+stopButton.classList.add("stop");
+
+stopButton.addEventListener("click", () => {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+});
+
+buttonsContainer.appendChild(stopButton);
